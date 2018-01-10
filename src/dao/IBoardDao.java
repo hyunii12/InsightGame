9a2e3b55ec52ac505731b6920557f54863508d95
student <student@M11016>
2018-01-10 17:34:38 +0900

    [현정] Game&Board - model, dao, dao.mapper

--- src/dao/IBoardDao.java
+++ src/dao/IBoardDao.java
@@ -1,5 +1,19 @@
 package dao;
 
-public interface IBoardDao {
+import java.util.List;
 
+import model.Board;
+
+public interface IBoardDao {
+	/*
+	 * Board - insert, update, delete, select(one, all)
+	 * select one : by bId, writer
+	 * select All : by *, header
+	 */
+	public int insertBoard(Board board);
+	public int updateBoard(Board board);
+	public int deleteBoard(int bId);
+	public Board selectBoardByBId(int bId);
+	public List<Board> selectBoardList();
+	public List<Board> selectBoardList(String header);
 }
