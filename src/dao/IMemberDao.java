9d15b6ef5c9b4d04a2ec5336face5fd32e8441e4
student <student@M110112>
2018-01-11 14:30:57 +0900

    member, streamer | dao, mapper

--- src/dao/IMemberDao.java
+++ src/dao/IMemberDao.java
@@ -1,5 +1,17 @@
 package dao;
 
+import java.util.List;
+
+import model.Member;
+
 public interface IMemberDao {
+	
+	public List<Member> selectAllMember();
+	public int insertMember(Member member);
+	public int updateMember(Member member);
+	public int deleteMember(int mId);
+	public Member selectOneMember(int mId);
+	public Member selectOneEmail(String mEmail);
+	
 
 }
--- src/service/MemberServiceImpl.java
+++ src/service/MemberServiceImpl.java
@@ -1,5 +1,67 @@
 package service;
 
-public class MemberServiceImpl implements IMemberService{
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.IMemberDao;
+import model.Member;
+
+@Service("memberService")
+public class MemberServiceImpl implements IMemberService{
+	
+	@Autowired
+	private IMemberDao memberDao;
+	
+	
+
+	@Override
+	public void join(Member member) {
+		// TODO Auto-generated method stub
+		memberDao.insertMember(member);
+	}
+
+
+	@Override
+	public boolean checkPassword(String mEmail, String mPassword) {
+		// TODO Auto-generated method stub
+		
+		Member member = memberDao.selectOneEmail(mEmail);
+		if(member.getmPasword().equals(mPassword))
+			return true;
+		else
+			return false;
+		
+	}
+
+	@Override
+	public Member getMemeber(int mId) {
+		// TODO Auto-generated method stub
+		return memberDao.selectOneMember(mId);
+	}
+
+	@Override
+	public List<Member> getMemberList() {
+		// TODO Auto-generated method stub
+		return memberDao.selectAllMember();
+	}
+
+
+	@Override
+	public int updateMember(Member member) {
+		// TODO Auto-generated method stub
+		return memberDao.updateMember(member);
+	}
+
+	@Override
+	public int deleteMember(int mId) {
+		// TODO Auto-generated method stub
+		return memberDao.deleteMember(mId);
+	}
+
+	
+	
+
+	
 }
