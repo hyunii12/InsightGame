66dc18b798d6bc082e0f47ef7903a24b50b3c448
student user <hyunii1227@gmail.com>
2018-01-12 12:33:19 +0900

    pages-menu.ts 주석

--- InsightGame_2/ngx-admin-master/src/app/pages/pages-menu.ts
+++ InsightGame_2/ngx-admin-master/src/app/pages/pages-menu.ts
@@ -11,41 +11,41 @@ export const MENU_ITEMS: NbMenuItem[] = [
     title: 'FEATURES',
     group: true,
   },
-  {
-    title: 'UI Features',
-    icon: 'nb-keypad',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
-    ],
-  },
+  // {
+  //   title: 'UI Features',
+  //   icon: 'nb-keypad',
+  //   link: '/pages/ui-features',
+  //   children: [
+  //     {
+  //       title: 'Buttons',
+  //       link: '/pages/ui-features/buttons',
+  //     },
+  //     {
+  //       title: 'Grid',
+  //       link: '/pages/ui-features/grid',
+  //     },
+  //     {
+  //       title: 'Icons',
+  //       link: '/pages/ui-features/icons',
+  //     },
+  //     {
+  //       title: 'Modals',
+  //       link: '/pages/ui-features/modals',
+  //     },
+  //     {
+  //       title: 'Typography',
+  //       link: '/pages/ui-features/typography',
+  //     },
+  //     {
+  //       title: 'Animated Searches',
+  //       link: '/pages/ui-features/search-fields',
+  //     },
+  //     {
+  //       title: 'Tabs',
+  //       link: '/pages/ui-features/tabs',
+  //     },
+  //   ],
+  // },
   {
     title: 'Forms',
     icon: 'nb-compose',
