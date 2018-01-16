4f88390c68e6facc33b150cf0505a7435d931da2
student <student@M110112>
2018-01-16 15:31:12 +0900

    ssss

--- src/controller/StreamerController.java
+++ src/controller/StreamerController.java
@@ -1,18 +1,17 @@
 package controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import service.IMemberService;
-import service.IStreamerService;
 
 @Controller
 public class StreamerController {
 	
-	@Autowired
-	private IStreamerService streamerService;
-	
-	
+	@RequestMapping("InsightStreamer.do")
+	public String main(Model model) {
+		System.out.println("여기는 스트리머페이지");
+		return "pages/streamer";
+	}
 }
