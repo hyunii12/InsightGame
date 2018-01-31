98af4b012408a288824540c9c5bbc67985553ef7
hyunjung <hyunii1227@gmail.com>
2018-01-31 13:41:01 +0900

    dddd

--- InsightGame/js/board.js
+++ InsightGame/js/board.js
@@ -219,7 +219,7 @@ function deleteBtn(bId){
 	});
 }
 function commentBtn(bId){
-	var targetTr = $('#tr_'+bId);
+	var targetTr = $('#tr_'+bId); 
 	if(targetTr.next('tr').attr('class') == 'writeComment'){
 		targetTr.next('tr').remove()
 		targetTr.find('button[name=commentBtn]').text('댓글')
